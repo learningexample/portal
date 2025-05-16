e7c03fbdc3e7a8826a1e174d3323198048678695
hello <work2@ksdfsdf.com>
2025-05-16 02:32:01 -0400

    icon

--- angular-portal/src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  apiUrl: null, // Set to null to indicate we're using hardcoded config
-  useHardcodedConfig: true
-};
--- angular-portal/src/environments/environment.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: false,
-  apiUrl: null, // Set to null to indicate we're using hardcoded config instead of an API
-  useHardcodedConfig: true
-};
